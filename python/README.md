03ae932bfeeda4c9a295d62535a9dab79f52363f
Emma Lam <emma.follette@gmail.com>
2016-07-21 05:31:36 -0700

    Made changes to READMEs

---
+++ python/README.md
@@ -0,0 +1,29 @@
+# Purpose
+Sample tools and utilities for LinkedIn Ads APIs
+
+### Installation
+To contribute to the project or use sample scripts, run the following commands in your terminal:
+
+
+```bash
+# clone the repository
+
+git clone git@github.com:emmma/ads-platform-tools.git
+cd python
+
+# install dependencies
+pip install -r requirements.txt
+```
+
+Set up `ACCESS_TOKEN` for your local machine.  Replace the `<value>` with the OAuth 2.0 Bearer token for the user (LinkedIn member) on whose behalf you'd like to make API calls
+
+```bash
+export ACCESS_TOKEN='<value>'
+```
+
+### Usage
+To run the python script, type the following command in terminal:
+
+```bash
+python smoke.py
+```
---
+++ python/smoke.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os
+import requests
+
+# OAuth 2.0 bearer token to make API calls on behalf of LinkedIn member
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+
+# LinkedIn API
+LINKEDIN_API_BASE_URL = 'https://api.linkedin.com/v1'
+
+def main():
+    results = get_user(ACCESS_TOKEN)
+    print results
+    return
+
+def get_user(token):
+    """
+    Returns the results from people API
+    """
+    headers = {
+        'Host': 'api.linkedin.com',
+        'Connection': 'Keep-Alive',
+        'Authorization': 'Bearer {token}'.format(token=token)
+    }
+
+    response = requests.get(LINKEDIN_API_BASE_URL + '/people/~?format=json', headers=headers)
+
+    if response is not None:
+        results = response.json()
+        user_first_name = results['firstName']
+        user_last_name = results['lastName']
+        user_headline = results['headline']
+    else:
+        results = None
+        user_first_name = None
+        user_last_name = None
+        user_headline = None
+    return user_first_name, user_last_name, user_headline
+
+if __name__ == '__main__':
+    main()
