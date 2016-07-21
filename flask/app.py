683533a052671731d486e2eec9d6e77a28fbae02
Emma Lam <emma.follette@gmail.com>
2016-07-21 04:47:14 -0700

    [Changed] parent folder

---
+++ flask/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+import client
+import server
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    client.prompt()
+    return render_template('index.html')
+
+@app.route('/callback', methods=['GET'])
+def oauth():
+    name = None
+    name = server.oauth(request)
+    return render_template('results.html', name=name)
+
+# TODO Create separate route so code and state is not displayed in URL
+
+if __name__ == '__main__':
+    main()
